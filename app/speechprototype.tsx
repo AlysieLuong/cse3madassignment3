e6f91bce317e7c740357726775d81d78f334a635
AlysieLuong <alysieluongg@gmail.com>
2026-05-02 23:53:07 +1000

    added comments for expo-speech features

--- app/speechprototype.tsx
+++ app/speechprototype.tsx
@@ -1,4 +1,4 @@
-import * as Speech from "expo-speech";
+import * as Speech from "expo-speech"; //expo-speech package for text-to-speech
 import { useState } from "react";
 import {
   Alert,
@@ -10,18 +10,26 @@ import {
 } from "react-native";
 
 export default function SpeechPrototype() {
+  //tracks if speech is playing
   const [isSpeaking, setIsSpeaking] = useState(false);
+
+  //stores user text input
   const [userInput, setUserInput] = useState("");
+
+  // stores number of available voices
   const [voiceCount, setVoiceCount] = useState<number | null>(null);
+
+  // stores first voice name/identifier
   const [firstVoice, setFirstVoice] = useState<string>("");
 
+  //defeault instructions if user doesn't input anything
   const instructions =
     "Welcome to the expo-speech prototype. Today we will explain what it does and complete a brief critical analysis on whether it would be useful for STEMM Lab.";
 
+  //retrieves available voices from devices
   const getVoices = async () => {
     try {
-      Alert.alert("Testing", "Button was pressed");
-
+      //expo-speech feature, fetch voices available
       const availableVoices = await Speech.getAvailableVoicesAsync();
 
       setVoiceCount(availableVoices.length);
@@ -49,9 +57,11 @@ export default function SpeechPrototype() {
     }
   };
 
+  //handles speaking text (default/user input)
   const speakInstructions = () => {
     const textToSpeak = userInput || instructions;
 
+    //expo-speech feature v, prevents speaking if text exceeds allowed length
     if (textToSpeak.length > Speech.maxSpeechInputLength) {
       Alert.alert("Error", "Text is too long to be spoken.");
       return;
@@ -59,16 +69,18 @@ export default function SpeechPrototype() {
 
     setIsSpeaking(true);
 
+    //expo-speech feature, converts selected tex string to spoken audio
     Speech.speak(textToSpeak, {
-      language: "en-AU",
-      rate: 0.9,
-      pitch: 1.0,
-      onDone: () => setIsSpeaking(false),
-      onStopped: () => setIsSpeaking(false),
-      onError: () => setIsSpeaking(false),
+      language: "en-AU", //expo-speech is a speechOption, code of a language to be used to read the text
+      rate: 0.9, //expo-speech is speechOption, rate of voice to speak 1.0 is normal rate
+      pitch: 1.0, // expo-speech is speechOption, pitch of voice to speak 1.0 is normal pitch
+      onDone: () => setIsSpeaking(false), //expo-speech is a speechOpton, callback invoked when speaking is finished
+      onStopped: () => setIsSpeaking(false), //expo-speech is a speechOption, callback invoked when speaking is stopped with Speech.stop()
+      onError: () => setIsSpeaking(false), // expo-speech is a speechOption, callback invoked when an error occurs while speaking
     });
   };
 
+  //expo-speech features, stops speech
   const stopSpeech = () => {
     Speech.stop();
     setIsSpeaking(false);
@@ -86,7 +98,7 @@ export default function SpeechPrototype() {
         placeholderTextColor="#aaa"
         value={userInput}
         onChangeText={setUserInput}
-        maxLength={Speech.maxSpeechInputLength}
+        maxLength={Speech.maxSpeechInputLength} //expo-speech, limits input length
         multiline
       />
 
